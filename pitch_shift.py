477a32576bf8c83d2a697a08c407df2ddb55b525
Lars Løberg Monstad <lars.monstad@gmail.com>
2023-04-05 11:35:47 +0200

    Create pitch_shift.py

---
+++ pitch_shift.py
@@ -0,0 +1,56 @@
+import os
+import argparse
+import librosa
+import numpy as np
+import soundfile as sf
+
+def load_ann_file(file_path):
+    with open(file_path, 'r') as f:
+        content = f.readlines()
+    return [line.strip() for line in content]
+
+def save_ann_file(file_path, content):
+    with open(file_path, 'w') as f:
+        f.write('\n'.join(content))
+
+def update_ann_file(ann_content, pitch_shift):
+    updated_content = []
+    for line in ann_content:
+        parts = line.strip().split('\t')
+        onset, offset, pitch, channel = parts
+        pitch_new = int(pitch) + pitch_shift
+        updated_line = f"{onset}\t{offset}\t{pitch_new}\t{channel}"
+        updated_content.append(updated_line)
+    return updated_content
+
+def apply_pitch_shift(audio_file, ann_file, output_dir, pitch_shift):
+    samples, sample_rate = librosa.load(audio_file, sr=None, mono=False)
+    pitch_shifted_samples = librosa.effects.pitch_shift(samples, sample_rate, n_steps=pitch_shift)
+
+    print(f"Input samples shape: {samples.shape}")
+    print(f"Output samples shape: {pitch_shifted_samples.shape}")
+    print(f"Pitch shift: {pitch_shift} semitones")
+
+    output_audio_file = os.path.join(output_dir, os.path.basename(audio_file))
+    sf.write(output_audio_file, pitch_shifted_samples.T, sample_rate, format='flac')
+
+    ann_content = load_ann_file(ann_file)
+    updated_ann_content = update_ann_file(ann_content, pitch_shift)
+    output_ann_file = os.path.join(output_dir, os.path.basename(ann_file))
+    save_ann_file(output_ann_file, updated_ann_content)
+
+def main():
+    parser = argparse.ArgumentParser(description="Pitch-shift audio and update annotation files")
+    parser.add_argument("input_audio_file", help="Path to the input audio file (FLAC or WAV)")
+    parser.add_argument("input_ann_file", help="Path to the input annotation file (.ann)")
+    parser.add_argument("output_directory", help="Path to the output directory")
+    parser.add_argument("pitch_shift", type=int, help="Pitch shift in semitones (e.g., 2 for two semitones up, -3 for three semitones down)")
+
+    args = parser.parse_args()
+
+    os.makedirs(args.output_directory, exist_ok=True)
+
+    apply_pitch_shift(args.input_audio_file, args.input_ann_file, args.output_directory, args.pitch_shift)
+
+if __name__ == "__main__":
+    main()
