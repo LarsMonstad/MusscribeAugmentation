e99cb7f568ba9455792919d3c8685eb146f115fb
Lars Løberg Monstad <lars.monstad@gmail.com>
2023-04-10 21:24:06 +0200

    fixed error, and added support for mono and stereo

--- distortion.py
+++ distortion.py
@@ -4,18 +4,14 @@ import librosa
 import numpy as np
 import soundfile as sf
 from pydub import AudioSegment
-from audiotsm import audio_effects
 
 def apply_distortion(audio_file, output_dir, gain):
     samples, sample_rate = librosa.load(audio_file, sr=None, mono=False)
-    input_audio = AudioSegment.from_mono_audiosegments(
-        AudioSegment.from_file(audio_file, format='flac', channels=1, frame_rate=sample_rate, sample_width=samples.dtype.itemsize * 8)[:samples.shape[1]],
-        AudioSegment.from_file(audio_file, format='flac', channels=1, frame_rate=sample_rate, sample_width=samples.dtype.itemsize * 8)[samples.shape[1]:]
-    )
+    channels = samples.shape[0]
 
-    samples = np.array(input_audio.get_array_of_samples()).reshape(-1, 2).T
-    distorted_samples = audio_effects.overdrive(samples, gain)
-    output_audio = AudioSegment(distorted_samples.tobytes(), frame_rate=sample_rate, sample_width=samples.dtype.itemsize * 8, channels=2)
+    input_audio = AudioSegment.from_file(audio_file, format='flac', channels=channels, frame_rate=sample_rate, sample_width=samples.dtype.itemsize * 8)
+    
+    output_audio = input_audio.apply_gain(gain)
     
     output_audio_file = os.path.join(output_dir, os.path.basename(audio_file))
     output_audio.export(output_audio_file, format='flac')
@@ -24,7 +20,7 @@ def main():
     parser = argparse.ArgumentParser(description="Apply distortion to audio files")
     parser.add_argument("input_audio_file", help="Path to the input audio file (FLAC or WAV)")
     parser.add_argument("output_directory", help="Path to the output directory")
-    parser.add_argument("gain", type=float, help="Gain for the overdrive effect")
+    parser.add_argument("gain", type=float, help="Gain for the distortion effect")
 
     args = parser.parse_args()
 
