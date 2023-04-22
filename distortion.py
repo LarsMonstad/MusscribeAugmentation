79714fca051e8bf6e12fbc70882ad806167f8e24
Lars Løberg Monstad <lars.monstad@gmail.com>
2023-04-22 02:33:52 +0200

    Update distortion.py
    added ann import, random word to fix overwriting and adde usage examples comments.

--- distortion.py
+++ distortion.py
@@ -1,11 +1,25 @@
 import os
 import argparse
+import random
+import string
 import librosa
 import numpy as np
 import soundfile as sf
 from pydub import AudioSegment
 
-def apply_distortion(audio_file, output_dir, gain):
+
+# range 8 high 3 low  0 off
+
+# Usage examples:
+# Distortion
+# 1. No Distortion python distortion.py input.flac input.ann outputfolder 0 
+# 2. Low Distortion python distortion.py input.flac input.ann outputfolder 3 
+# 3. High Distortion python distortion.py input.flac input.ann outputfolder 8 
+
+def random_word(length):
+    return ''.join(random.choice(string.ascii_lowercase) for _ in range(length))
+
+def apply_distortion(audio_file, output_audio_file, gain):
     samples, sample_rate = librosa.load(audio_file, sr=None, mono=False)
     channels = samples.shape[0]
 
@@ -13,20 +27,29 @@ def apply_distortion(audio_file, output_dir, gain):
     
     output_audio = input_audio.apply_gain(gain)
     
-    output_audio_file = os.path.join(output_dir, os.path.basename(audio_file))
     output_audio.export(output_audio_file, format='flac')
 
 def main():
     parser = argparse.ArgumentParser(description="Apply distortion to audio files")
     parser.add_argument("input_audio_file", help="Path to the input audio file (FLAC or WAV)")
+    parser.add_argument("input_ann_file", help="Path to the input annotation file (.ann)")
     parser.add_argument("output_directory", help="Path to the output directory")
     parser.add_argument("gain", type=float, help="Gain for the distortion effect")
 
     args = parser.parse_args()
 
     os.makedirs(args.output_directory, exist_ok=True)
+    input_filename = os.path.basename(args.input_audio_file)
+    random_suffix = random_word(5)
+    output_filename = os.path.splitext(input_filename)[0] + "_distortion_" + random_suffix + os.path.splitext(input_filename)[1]
+    output_file_path = os.path.join(args.output_directory, output_filename)
 
-    apply_distortion(args.input_audio_file, args.output_directory, args.gain)
+    apply_distortion(args.input_audio_file, output_file_path, args.gain)
+
+    # Copy the annotation file to the output directory without modifying it
+    output_ann_filename = os.path.splitext(os.path.basename(args.input_ann_file))[0] + "_distortion_" + random_suffix + os.path.splitext(args.input_ann_file)[1]
+    output_ann_file_path = os.path.join(args.output_directory, output_ann_filename)
+    os.system(f'cp "{args.input_ann_file}" "{output_ann_file_path}"')
 
 if __name__ == "__main__":
     main()
