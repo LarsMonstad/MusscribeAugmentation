e477e0f139cdc50213f9510d2967498dcd4f2e6e
Lars Løberg Monstad <lars.monstad@gmail.com>
2023-04-22 03:53:51 +0200

    Update distortion.py

--- distortion.py
+++ distortion.py
@@ -14,20 +14,35 @@ from pydub import AudioSegment
 # Distortion
 # 1. No Distortion python distortion.py input.flac input.ann outputfolder 0 
 # 2. Low Distortion python distortion.py input.flac input.ann outputfolder 3 
-# 3. High Distortion python distortion.py input.flac input.ann outputfolder 8 
+# 3. High Distortion python distortion.py input.flac input.ann output 8 
 
 def random_word(length):
     return ''.join(random.choice(string.ascii_lowercase) for _ in range(length))
 
-def apply_distortion(audio_file, output_audio_file, gain):
-    samples, sample_rate = librosa.load(audio_file, sr=None, mono=False)
+def generate_output_filename(input_filename, suffix):
+    return os.path.splitext(input_filename)[0] + "_" + suffix + os.path.splitext(input_filename)[1]
+
+def apply_distortion(input_audio_file, input_ann_file, output_directory, gain):
+    random_suffix = random_word(5)
+    os.makedirs(output_directory, exist_ok=True)
+
+    # Generate output audio file name and path
+    output_audio_filename = generate_output_filename(os.path.basename(input_audio_file), "distortion_" + random_suffix)
+    output_audio_file_path = os.path.join(output_directory, output_audio_filename)
+
+    samples, sample_rate = librosa.load(input_audio_file, sr=None, mono=False)
     channels = samples.shape[0]
 
-    input_audio = AudioSegment.from_file(audio_file, format='flac', channels=channels, frame_rate=sample_rate, sample_width=samples.dtype.itemsize * 8)
-    
+    input_audio = AudioSegment.from_file(input_audio_file, format='flac', channels=channels, frame_rate=sample_rate, sample_width=samples.dtype.itemsize * 8)
+
     output_audio = input_audio.apply_gain(gain)
-    
-    output_audio.export(output_audio_file, format='flac')
+
+    output_audio.export(output_audio_file_path, format='flac')
+
+    # Copy the annotation file to the output directory with a modified name
+    output_ann_filename = generate_output_filename(os.path.basename(input_ann_file), "distortion_" + random_suffix)
+    output_ann_file_path = os.path.join(output_directory, output_ann_filename)
+    os.system(f'cp "{input_ann_file}" "{output_ann_file_path}"')
 
 def main():
     parser = argparse.ArgumentParser(description="Apply distortion to audio files")
@@ -39,17 +54,9 @@ def main():
     args = parser.parse_args()
 
     os.makedirs(args.output_directory, exist_ok=True)
-    input_filename = os.path.basename(args.input_audio_file)
-    random_suffix = random_word(5)
-    output_filename = os.path.splitext(input_filename)[0] + "_distortion_" + random_suffix + os.path.splitext(input_filename)[1]
-    output_file_path = os.path.join(args.output_directory, output_filename)
 
-    apply_distortion(args.input_audio_file, output_file_path, args.gain)
-
-    # Copy the annotation file to the output directory without modifying it
-    output_ann_filename = os.path.splitext(os.path.basename(args.input_ann_file))[0] + "_distortion_" + random_suffix + os.path.splitext(args.input_ann_file)[1]
-    output_ann_file_path = os.path.join(args.output_directory, output_ann_filename)
-    os.system(f'cp "{args.input_ann_file}" "{output_ann_file_path}"')
+    apply_distortion(args.input_audio_file, args.input_ann_file, args.output_directory, args.gain)
 
 if __name__ == "__main__":
     main()
+
