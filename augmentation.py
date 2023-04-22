102519f9b79dddc26b4a940e80ba3db96b214574
Lars Løberg Monstad <lars.monstad@gmail.com>
2023-04-22 03:54:29 +0200

    Update augmentation.py
    fixed a lot of bugs

--- augmentation.py
+++ augmentation.py
@@ -1,9 +1,17 @@
 import os
 import argparse
-from time_stretch import apply_time_stretch
+import random
+import string
+from tempoaug2 import apply_time_stretch
 from pitch_shift import apply_pitch_shift
-from reverb import apply_reverb
+from reverbfilter import apply_reverb_and_filters
 from distortion import apply_distortion
+
+def random_word(length):
+    return ''.join(random.choice(string.ascii_lowercase) for _ in range(length))
+
+def generate_output_filename(input_filename, effect_name, measure, random_suffix):
+    return f"{os.path.splitext(input_filename)[0]}_{effect_name}_{measure}_{random_suffix}{os.path.splitext(input_filename)[1]}"
 
 def main():
     parser = argparse.ArgumentParser(description="Apply audio effects to audio and annotation files")
@@ -13,27 +21,36 @@ def main():
 
     args = parser.parse_args()
 
-    os.makedirs(args.output_directory, exist_ok=True)
+    # Use the input file's directory as the output directory
+    output_directory = args.output_directory
 
     # Apply time stretch
     for i, stretch_factor in enumerate([0.8, 1.2, 1.4]):
-        output_subdir = os.path.join(args.output_directory, f"timestretch_{i+1}")
-        apply_time_stretch(args.input_audio_file, args.input_ann_file, output_subdir, stretch_factor)
+        random_suffix = random_word(5)
+        output_filename = generate_output_filename(os.path.basename(args.input_audio_file), "timestretch", stretch_factor, random_suffix)
+        output_file_path = os.path.join(output_directory, output_filename)
+        apply_time_stretch(args.input_audio_file, args.input_ann_file, output_file_path, stretch_factor)
 
     # Apply pitch shift
     for i, semitones in enumerate([-2, 2, 4]):
-        output_subdir = os.path.join(args.output_directory, f"pitchshift_{i+1}")
-        apply_pitch_shift(args.input_audio_file, args.input_ann_file, output_subdir, semitones)
+        random_suffix = random_word(5)
+        output_filename = generate_output_filename(os.path.basename(args.input_audio_file), "pitchshift", semitones, random_suffix)
+        output_file_path = os.path.join(output_directory, output_filename)
+        apply_pitch_shift(args.input_audio_file, args.input_ann_file, output_file_path, semitones)
 
-    # Apply reverb
-    for i, room_scale in enumerate([0.3, 0.6, 0.9]):
-        output_subdir = os.path.join(args.output_directory, f"reverb_{i+1}")
-        apply_reverb(args.input_audio_file, output_subdir, room_scale)
+    # Apply reverb and filters
+    room_scales = [0, 30, 100]
+    low_cutoffs = [20000, 16300, 17500]
+    high_cutoffs = [20, 4000, 7000]
+
+    for i, room_scale in enumerate(room_scales):
+        low_cutoff = random.choice(low_cutoffs)
+        high_cutoff = random.choice(high_cutoffs)
+        apply_reverb_and_filters(args.input_audio_file, args.input_ann_file, output_directory, room_scale, low_cutoff, high_cutoff)
 
     # Apply distortion
-    for i, gain in enumerate([10, 20, 30]):
-        output_subdir = os.path.join(args.output_directory, f"distortion_{i+1}")
-        apply_distortion(args.input_audio_file, output_subdir, gain)
+    for i, gain in enumerate([3, 5, 8]):
+        apply_distortion(args.input_audio_file, args.input_ann_file, output_directory, gain)
 
 if __name__ == "__main__":
     main()
