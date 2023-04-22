adfda17b0f8caf0ba49e2475a0f708bc9af2ebe9
Lars Løberg Monstad <lars.monstad@gmail.com>
2023-04-22 17:55:49 +0200

    Update augmentation.py
    fixed annmidi convertion to only convert the ann files created

--- augmentation.py
+++ augmentation.py
@@ -15,14 +15,14 @@ def random_word(length):
 def generate_output_filename(input_filename, effect_name, measure, random_suffix):
     return f"{os.path.splitext(input_filename)[0]}_{effect_name}_{measure}_{random_suffix}{os.path.splitext(input_filename)[1]}"
 
-def convert_ann_files(directory):
-    for filename in tqdm(os.listdir(directory), desc="Coverting ann to midi "):
-        if filename.endswith(".ann"):
-            ann_file_path = os.path.join(directory, filename)
-            ann_to_midi(ann_file_path)
-            # Call your conversion function here with ann_file_path as the argument
-            # For example:
-            # convert_ann_file(ann_file_path)
+#def convert_ann_files(directory):
+#    for filename in tqdm(os.listdir(directory), desc="Coverting ann to midi "):
+#        if filename.endswith(".ann"):
+#            ann_file_path = os.path.join(directory, filename)
+#            ann_to_midi(ann_file_path)
+#            # Call your conversion function here with ann_file_path as the argument
+#            # For example:
+#            # convert_ann_file(ann_file_path)
 
 def main():
     parser = argparse.ArgumentParser(description="Apply audio effects to audio and annotation files")
@@ -35,19 +35,25 @@ def main():
     # Use the input file's directory as the output directory
     output_directory = args.output_directory
 
+    #create a list of ann files 
+    new_ann_files = []
+
     # Apply time stretch
     for i, stretch_factor in tqdm(enumerate([0.8, 1.2, 1.4]), desc="Time Stretch"):
         random_suffix = random_word(5)
         output_filename = generate_output_filename(os.path.basename(args.input_audio_file), "timestretch", stretch_factor, random_suffix)
         output_file_path = os.path.join(output_directory, output_filename)
-        apply_time_stretch(args.input_audio_file, args.input_ann_file, output_file_path, stretch_factor)
+        output_ann_file = apply_time_stretch(args.input_audio_file, args.input_ann_file, output_file_path, stretch_factor)
+        new_ann_files.append(output_ann_file)
+
 
     # Apply pitch shift
     for i, semitones in tqdm(enumerate([-2, 2, 4]), desc="Pitch Shift"):
         random_suffix = random_word(5)
         output_filename = generate_output_filename(os.path.basename(args.input_audio_file), "pitchshift", semitones, random_suffix)
         output_file_path = os.path.join(output_directory, output_filename)
-        apply_pitch_shift(args.input_audio_file, args.input_ann_file, output_file_path, semitones)
+        output_ann_file = apply_pitch_shift(args.input_audio_file, args.input_ann_file, output_file_path, semitones)
+        new_ann_files.append(output_ann_file)
 
     # Apply reverb and filters
     room_scales = [0, 30, 100]
@@ -57,7 +63,8 @@ def main():
     for i, room_scale in tqdm(enumerate(room_scales), desc="Reverb and filter"):
         low_cutoff = random.choice(low_cutoffs)
         high_cutoff = random.choice(high_cutoffs)
-        apply_reverb_and_filters(args.input_audio_file, args.input_ann_file, output_directory, room_scale, low_cutoff, high_cutoff)
+        output_ann_file = apply_reverb_and_filters(args.input_audio_file, args.input_ann_file, output_directory, room_scale, low_cutoff, high_cutoff)
+        new_ann_files.append(output_ann_file)
 
     # Apply distortion
     depths = [0.2, 0.3, 0.5]
@@ -65,9 +72,12 @@ def main():
     for i, gain in tqdm(enumerate([3, 5, 10]), desc= "Reverb and filter"):
         depth = random.choice(depths)
         chorusrate = random.choice(chorusrates)
-        apply_gain_and_chorus(args.input_audio_file, args.input_ann_file, output_directory, gain, depth, chorusrate)
+        output_ann_file = apply_gain_and_chorus(args.input_audio_file, args.input_ann_file, output_directory, gain, depth, chorusrate)
+        new_ann_files.append(output_ann_file)
 
-    convert_ann_files(output_directory)
+    #convert_ann_files(output_directory)
+    for ann_file in tqdm(new_ann_files, desc= "Converting ann files to midi"):
+        ann_to_midi(ann_file)
 
 if __name__ == "__main__":
     main()
